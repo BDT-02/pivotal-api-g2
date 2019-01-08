68bbb55aaa3787c52659c2784241974bf3ecbff4
Sandra91 <ternukis.91rf@gmail.com>
2019-01-08 18:20:24 -0400

    adding steps

--- features/workspaces.feature
+++ features/workspaces.feature
@@ -1,13 +1,32 @@
-Feature: showing off behave
+Feature: Working workspace
 
   Scenario: Create new workspace
     Given I create a workspace
-      | name | First Workspace |
+      | name            |
+      | First Workspace |
     Then I verify workspace creation status is 200
     And I verify workspace schema
 
-  Scenario: Get project
+  Scenario: Get workspace
     Given I create a workspace
-      | name | First Workspace |
+      | name            |
+      | First Workspace |
     Then I verify workspace creation status is 200
     And I verify workspace schema
+
+  Scenario: Update the workspace
+    Given I create a workspace
+      | name          |
+      | First workspace |
+    When I update the workspace
+      | name           |
+      | Update workspace |
+    Then I verify workspace updated status is 200
+    And I verify workspace schema
+
+  Scenario: Delete the workspace
+    Given I create a workspace
+      | name          |
+      | First workspace |
+    When I delete the workspace
+    Then I verify workspace deleted status is 204
--- src/pivotal_api_services/workspaces.py
+++ src/pivotal_api_services/workspaces.py
@@ -5,39 +5,40 @@ from src.utils.string_handler import StringHandler
 
 logger = LoggerHandler.get_instance()
 
-class ProjectServices(PivotalServices):
+
+class WorkspaceServices(PivotalServices):
 
     def __init__(self):
         super(ProjectServices, self).__init__()
         self.__workspaces = "{}/workspaces".format(self.request_handler.main_url)
-        self.__workspaces_schema_path = "/src/core/api/json_schemas/workspaces_schema.json"
-        self.__workspaces = {}
-        self.__workspaces = {}
+        self.__workspaces_schema_path = "/src/core/api/json_schemas/workspace_schema.json"
+        self.workspace = {}
+        self.workspaces = {}
 
     def create_workspace(self, data):
         response = self.request_handler.post_request(endpoint=self.__workspaces, body=data)
         return response.status_code, response.json()
 
-    def get_workspace(self):
+    def get_workspaces(self):
         workspaces_list = self.request_handler.get_request(endpoint=self.__workspaces).json()
         for workspace in workspaces_list:
             if not workspace['name'] in self.workspace:
                 self.workspace[workspace['name']] = workspace['id']
         return self.workspace
 
-    def get_project(self, id):
-        current_url = self.__workspace + "/" + id
+    def get_workspace(self, id):
+        current_url = self.__workspaces + "/" + id
         workspace = self.request_handler.get_request(endpoint=current_url).json()
         if not workspace['name'] in self.workspace:
             self.workspaces[workspace['name']] = workspace['id']
         return workspace
 
     def get_workspaces_schema(self):
-        return StringHandler.convert_string_to_json(FileReader.get_file_content(self.__workspace_schema_path))
+        return StringHandler.convert_string_to_json(FileReader.get_file_content(self.__workspaces_schema_path))
 
     def delete_all_workspace(self):
         self.get_workspace()
         for workspace in self.workspace.values():
-            url = self.__workspace + "/" + str(workspace)
+            url = self.__workspaces + "/" + str(workspace)
             logger.info("Deleting %s" % url)
             self.request_handler.delete_request(endpoint=url)
